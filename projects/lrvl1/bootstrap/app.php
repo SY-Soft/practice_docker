089ff702a86d25b6060e6cbf4e57873cfafcb76c
SY-Soft <sy.soft.work@gmail.com>
2026-05-10 12:46:50 +0300

    поддерживает Laravel поддерживает Drupal 8/9/10/11 автоматически определяет web root

--- projects/lrvl1/bootstrap/app.php
+++ projects/lrvl1/bootstrap/app.php
@@ -3,6 +3,8 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+//use Illuminate\Auth\Access\AuthorizationException;
+
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -14,5 +16,5 @@ return Application::configure(basePath: dirname(__DIR__))
         //
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        //
+
     })->create();
